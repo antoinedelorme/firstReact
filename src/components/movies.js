6dc5afb22cc1681e80b77c0c0a7dec8a70e637cf
antoinedelorme <antoine.delorme@me.com>
2020-09-30 19:27:25 +0200

    v1.1

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default () => {
+  const [query, setQuery] = useState("");
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("THIS WORKS");
+    const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=8d16b00d800271d1d037c35d5d3df0e6&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.results[0]);
+  };
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Movie Name
       </label>
@@ -11,6 +23,11 @@ export default () => {
         type="text"
         name="query"
         placeholder="i.e. test Park"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+          console.log(query);
+        }}
       />
       <button className="button" type="submit">
         Search
