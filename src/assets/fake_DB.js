52f46e94feca9dac4fdc6138bf18a1c3e03f6aa1
antoinedelorme <antoine.delorme@me.com>
2020-10-02 11:30:40 +0200

    1.9

--- src/assets/fake_DB.js
+++ src/assets/fake_DB.js
@@ -1,27 +1,41 @@
+const date0 = new Date(2020, 10, 2, 15, 0);
+const date1 = new Date(2020, 10, 11, 11, 0);
+const date2 = new Date(2020, 10, 4, 9, 0);
+const date3 = new Date(2020, 11, 5, 15, 0);
+const date4 = new Date(2020, 10, 6, 14, 0);
+const date5 = new Date(2020, 10, 7, 17, 0);
+const date6 = new Date(2020, 10, 8, 15, 0);
+const date7 = new Date(2020, 10, 9, 15, 0);
+const date8 = new Date(2020, 10, 10, 20, 0);
+
 const events = [
   {
     id: 1,
     title1: "Van Gogh",
     title2: "musée d'Orsay",
-    src: "/images/img1.png"
+    src: "/images/img1.png",
+    date: date0
   },
   {
     id: 2,
     title1: "Intelligence artificielle",
     title2: "mythes et réalités",
-    src: "/images/img2.png"
+    src: "/images/img2.png",
+    date: date1
   },
   {
     id: 3,
     title1: "San Francisco",
     title2: "lieux insolites",
-    src: "/images/img3.png"
+    src: "/images/img3.png",
+    date: date2
   },
   {
     id: 4,
     title1: "Hokusai",
     title2: "musée Guimet",
-    src: "/images/img4.png"
+    src: "/images/img4.png",
+    date: date3
   }
 ];
 
--- src/components/image.js
+++ src/components/image.js
@@ -1,13 +1,43 @@
 import React from "react";
 
 export default ({ event }) => {
-  console.log(event);
+  const { date } = event;
+  const weekday = new Array(7);
+  weekday[0] = "DIM";
+  weekday[1] = "LUN";
+  weekday[2] = "MAR";
+  weekday[3] = "MER";
+  weekday[4] = "JEU";
+  weekday[5] = "VEN";
+  weekday[6] = "SAM";
+
+  const monthName = new Array(12);
+  monthName[0] = "JAN";
+  monthName[1] = "FEV";
+  monthName[2] = "MAR";
+  monthName[3] = "AVR";
+  monthName[4] = "MAI";
+  monthName[5] = "JUN";
+  monthName[6] = "JUL";
+  monthName[7] = "AOU";
+  monthName[8] = "SEP";
+  monthName[9] = "OCT";
+  monthName[10] = "NOV";
+  monthName[11] = "DEC";
+
+  const n = weekday[date.getDay()];
+
   return (
     <div className="imageContainer">
       <img width="100%" src={event.src} alt="" />
       <div className="imageContainerBottom"></div>
       <div className="imageContainerTitle1">{event.title1}</div>
       <div className="imageContainerTitle2">{event.title2}</div>
+      <div className="imageDateContainer">
+        <div className="imageMonthFormat">{n}</div>
+        <div className="imageDateFormat">{date.getDay()}</div>
+        <div>{monthName[date.getMonth() - 1]}</div>
+      </div>
     </div>
   );
 };
