806c360275af06c5bbc0382ee5deebf25ddc53cb
antoinedelorme <antoine.delorme@me.com>
2020-10-02 16:11:53 +0200

    pre useState

--- src/App.js
+++ src/App.js
@@ -5,18 +5,33 @@ import events from "./assets/fake_DB";
 import SearchMovies from "./components/movies";
 import ImageComponent from "./components/image";
 import EventComponent from "./components/event";
+import { Button, ButtonGroup } from "@material-ui/core";
 
 export default function App() {
   return (
     <div className="App">
       <SearchMovies />
+      <div className="ContainerTitle">Pour vous</div>
       <div className="imagesContainer">
-        {events.map((event) => (
+        {events.slice(0, 5).map((event) => (
           <ImageComponent key={event.id} event={event} />
         ))}
       </div>
+      <div className="filtersContainer">
+        <ButtonGroup
+          size="large"
+          color="primary"
+          aria-label="large outlined primary button group"
+        >
+          <Button style={{ fontSize: "0.5em" }}>Histoire</Button>
+          <Button style={{ fontSize: "0.5em" }}>Musique</Button>
+          <Button style={{ fontSize: "0.5em" }}>Art</Button>
+          <Button style={{ fontSize: "0.5em" }}>Sciences</Button>
+          <Button style={{ fontSize: "0.5em" }}>Voyages</Button>
+        </ButtonGroup>
+      </div>
+
       <div className="eventsContainer">
-        
         {events.map((event) => (
           <EventComponent key={event.id} event={event} />
         ))}
--- src/assets/fake_DB.js
+++ src/assets/fake_DB.js
@@ -14,28 +14,78 @@ const events = [
     title1: "Van Gogh",
     title2: "musée d'Orsay",
     src: "/images/img1.png",
-    date: date0
+    date: date0,
+    type: "art"
   },
   {
     id: 2,
     title1: "Intelligence artificielle",
     title2: "mythes et réalités",
     src: "/images/img2.png",
-    date: date1
+    date: date1,
+    type: "science"
   },
   {
     id: 3,
     title1: "San Francisco",
     title2: "lieux insolites",
     src: "/images/img3.png",
-    date: date2
+    date: date2,
+    type: "voyages"
   },
   {
     id: 4,
     title1: "Hokusai",
     title2: "musée Guimet",
     src: "/images/img4.png",
-    date: date3
+    date: date3,
+    type: "art"
+  },
+  {
+    id: 5,
+    title1: "Le bal des philosophes",
+    title2: "Bal Blomet",
+    src: "/images/img5.png",
+    date: date4,
+    type: "musique"
+  },
+  {
+    id: 6,
+    title1: "Ragiroa",
+    title2: "voyage au bout du monde",
+    src: "/images/img6.png",
+    date: date7,
+    type: "voyages"
+  },
+  {
+    id: 7,
+    title1: "La feijoada",
+    title2: "apprenez avec un chef",
+    src: "/images/img7.png",
+    date: date7,
+    type: "cuisine"
+  },
+  {
+    id: 8,
+    title1: "Gengis Khan",
+    title2: "le loup bleu",
+    src: "/images/img8.png",
+    date: date3,
+    type: "histoire"
+  },
+  {
+    id: 9,
+    title1: "Loisirs",
+    title2: "un potager en ville",
+    src: "/images/img9.png",
+    date: date8
+  },
+  {
+    id: 10,
+    title1: "Pierre Soulages",
+    title2: "musée pompidou",
+    src: "/images/img10.png",
+    date: date8
   }
 ];
 
