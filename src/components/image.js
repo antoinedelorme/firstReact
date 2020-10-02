97924b6b6e9bab09db2f312e1bc8d29ee5c9ba32
antoinedelorme <antoine.delorme@me.com>
2020-10-02 07:47:21 +0200

    1.5

--- src/components/image.js
+++ src/components/image.js
@@ -4,8 +4,7 @@ export default ({ event }) => {
   console.log(event);
   return (
     <div className="imageContainer">
-      <img width="250px" height="250px" src={event.src} alt="" />
-      <div className="imageContainerBottom">toto tooto totototototototo</div>
+      <img width="50%" src={event.src} alt="" />
       <div className="imageContainerTitle1">{event.title1}</div>
       <div className="imageContainerTitle2">{event.title2}</div>
     </div>
