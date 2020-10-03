ad1a9cdd60118b2d51730cef316bfc6568b6ced4
antoinedelorme <antoine.delorme@me.com>
2020-10-03 17:21:29 +0200

    update

--- src/event.js
+++ src/event.js
@@ -3,5 +3,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => {
-  return <h1> TEST</h1>;
+  const mainContainerStyle = {
+    display: "flex",
+    justifyContent: "evenly-spaced"
+  };
+  const leftContainerStyle = {
+    backgroundColor: "#d1ccc0",
+    flex: 2,
+    height: "300px",
+    margin: "10em"
+  };
+  const rightContainerStyle = {
+    backgroundColor: "#d1ccc0",
+    flex: 3,
+    height: "300px",
+    margin: "10px"
+  };
+
+  return (
+    <div style={mainContainerStyle}>
+      <div style={leftContainerStyle}>
+        <h1>LEFT</h1>
+      </div>
+      <div style={rightContainerStyle}>
+        {" "}
+        <h1>RIGHT</h1>
+      </div>
+    </div>
+  );
 };
