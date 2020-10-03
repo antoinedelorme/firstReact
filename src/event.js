2d4749020c36d27d263315eda2f11da11325e89f
antoinedelorme <antoine.delorme@me.com>
2020-10-03 19:24:55 +0200

    temporary

--- src/event.js
+++ src/event.js
@@ -11,13 +11,17 @@ export default () => {
     backgroundColor: "#d1ccc0",
     flex: 2,
     height: "300px",
-    margin: "10em"
+    marginTop: "3em",
+    marginLeft: "5em",
+    marginRight: "2.5em"
   };
   const rightContainerStyle = {
     backgroundColor: "#d1ccc0",
     flex: 3,
     height: "300px",
-    margin: "10px"
+    marginTop: "3em",
+    marginRight: "5em",
+    marginLeft: "2.5em"
   };
 
   return (
